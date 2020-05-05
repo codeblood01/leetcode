370517e37fda7aa5ceb668e6d0e61b8c89937db5
gourav <gouravbhatt744@gmail.com>
2020-05-06 00:26:30 +0530

    removed set usage as we need to check wether a element comes first time, or doesnt came at all or came more than two times(represnt three states, 2 bits required), so we can use arraylist of bitset  ?

--- src/com/gourav/leetcode/may30days/leetcode/editor/en/FirstUniqueCharacterInAString.java
+++ src/com/gourav/leetcode/may30days/leetcode/editor/en/FirstUniqueCharacterInAString.java
@@ -19,28 +19,51 @@
 	
 package com.gourav.leetcode.may30days.leetcode.editor.en;
 
-import java.util.HashSet;
-import java.util.Set;
+//setimport java.sql.Array;
+import java.util.*;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int firstUniqChar(String s) {
         boolean arr[]=new boolean[26];
-        Set<Character> set= new HashSet<Character>();
+        List<BitSet> set= new ArrayList<BitSet>();
         char stringToChar[]= s.toCharArray();
+        for(int i=0; i < 26;i++){
+            BitSet initial = new BitSet(2);
+            set.add(initial);
+        }
+
         for(int i=0; i< stringToChar.length; i++ ){
-            if(set.add(stringToChar[i])){
-                arr[stringToChar[i]-97]=true;
-            }else{
-                arr[stringToChar[i]-97]=false;
+            if(!set.get(stringToChar[i]-97).get(0)&&!set.get(stringToChar[i]-97).get(1)){
+                set.get(stringToChar[i]-97).flip(1);
+            }else if (!set.get(stringToChar[i]-97).get(0)&&set.get(stringToChar[i]-97).get(1)){
+                set.get(stringToChar[i]-97).flip(0);
             }
         }
         for (int i = 0; i < stringToChar.length; i++) {
-            if (arr[stringToChar[i]-97])
-                    return i;
+            if (!set.get(stringToChar[i]-97).get(0)&&set.get(stringToChar[i]-97).get(1))
+                return i;
         }
         return -1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
-	
+//above is more efficient then this
+//removed set usage as we need to check wether a element comes first time, or doesnt came at all or came more than two times(represnt three states, 2 bits required), so we can use arraylist of bitset  ?
+//public int firstUniqChar(String s) {
+//    boolean arr[]=new boolean[26];
+//    Set<Character> set= new HashSet<Character>();
+//    char stringToChar[]= s.toCharArray();
+//    for(int i=0; i< stringToChar.length; i++ ){
+//        if(set.add(stringToChar[i])){
+//            arr[stringToChar[i]-97]=true;
+//        }else{
+//            arr[stringToChar[i]-97]=false;
+//        }
+//    }
+//    for (int i = 0; i < stringToChar.length; i++) {
+//        if (arr[stringToChar[i]-97])
+//            return i;
+//    }
+//    return -1;
+//}
