fb4e79ae2b9bff3c46ad144a64be31893f6bf309
gourav <gouravbhatt744@gmail.com>
2020-05-16 22:05:28 +0530

    why stack approach like maximum horizontanl bargraph rectangale, dont use min range by matrix(n^2)/segment tree

--- src/com/gourav/leetcode/may30days/leetcode/editor/en/RemoveKDigits.java
+++ src/com/gourav/leetcode/may30days/leetcode/editor/en/RemoveKDigits.java
@@ -41,107 +41,95 @@
 package com.gourav.leetcode.may30days.leetcode.editor.en;
 
 import java.util.HashMap;
+import java.util.Stack;
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public String removeKdigits(String num, int k) {
-        HashMap<Integer,Integer > indexMap = new HashMap<>();
-        int arr[]=new int[num.length()];
-        char charArr[]= num.toCharArray();
-        if(k==num.length())
-            return "0";
-        for(int i=0; i < num.length();i++){
-            arr[i]=charArr[i]-'0';
-            indexMap.
+        Stack<Character> stack = new Stack<Character>();
+        char arr[]=num.toCharArray();
+        stack.add(arr[0]);
+        String str="";
+        boolean flag=false;
+        for (int i = 1; i < arr.length; i++) {
+            flag=true;
+            while (k>0&&!stack.isEmpty()&&stack.peek()>arr[i]){
+                stack.pop();
+                k--;
+            }
+            stack.add(arr[i]);
         }
+      while (k>0&&!stack.isEmpty()){
+          stack.pop();
+          k--;
+      }
+        while (!stack.isEmpty()){
+            str=stack.pop()+str;
+            k--;
+        }
+        String result=removeZero(str);
+        return result.length()>0?result:"0";
     }
+    public  String removeZero(String str)
+        {
+        // Count leading zeros
+        int i = 0;
+        while (i < str.length() && str.charAt(i) == '0')
+        i++;
+
+        // Convert str into StringBuffer as Strings
+        // are immutable.
+        StringBuffer sb = new StringBuffer(str);
+
+        // The  StringBuffer replace function removes
+        // i characters from given index (0 here)
+        sb.replace(0, i, "");
+
+        return sb.toString();  // return in String
+        }
 }
-class SegmentTreeRMQ {
-    int st[]; //array to store segment tree 
 
-    // A utility function to get minimum of two numbers 
-    int minVal(int x, int y) {
-        return (x < y) ? x : y;
-    }
-
-    // A utility function to get the middle index from corner 
-    // indexes. 
-    int getMid(int s, int e) {
-        return s + (e - s) / 2;
-    }
-
-    /*  A recursive function to get the minimum value in a given 
-        range of array indexes. The following are parameters for 
-        this function. 
-  
-        st    --> Pointer to segment tree 
-        index --> Index of current node in the segment tree. Initially 
-                   0 is passed as root is always at index 0 
-        ss & se  --> Starting and ending indexes of the segment 
-                     represented by current node, i.e., st[index] 
-        qs & qe  --> Starting and ending indexes of query range */
-    int RMQUtil(int ss, int se, int qs, int qe, int index) {
-        // If segment of this node is a part of given range, then 
-        // return the min of the segment 
-        if (qs <= ss && qe >= se)
-            return st[index];
-
-        // If segment of this node is outside the given range 
-        if (se < qs || ss > qe)
-            return Integer.MAX_VALUE;
-
-        // If a part of this segment overlaps with the given range 
-        int mid = getMid(ss, se);
-        return minVal(RMQUtil(ss, mid, qs, qe, 2 * index + 1),
-                RMQUtil(mid + 1, se, qs, qe, 2 * index + 2));
-    }
-
-    // Return minimum of elements in range from index qs (query 
-    // start) to qe (query end).  It mainly uses RMQUtil() 
-    int RMQ(int n, int qs, int qe) {
-        // Check for erroneous input values 
-        if (qs < 0 || qe > n - 1 || qs > qe) {
-            System.out.println("Invalid Input");
-            return -1;
-        }
-
-        return RMQUtil(0, n - 1, qs, qe, 0);
-    }
-
-    // A recursive function that constructs Segment Tree for 
-    // array[ss..se]. si is index of current node in segment tree st 
-    int constructSTUtil(int arr[], int ss, int se, int si) {
-        // If there is one element in array, store it in current 
-        //  node of segment tree and return 
-        if (ss == se) {
-            st[si] = arr[ss];
-            return arr[ss];
-        }
-
-        // If there are more than one elements, then recur for left and 
-        // right subtrees and store the minimum of two values in this node 
-        int mid = getMid(ss, se);
-        st[si] = minVal(constructSTUtil(arr, ss, mid, si * 2 + 1),
-                constructSTUtil(arr, mid + 1, se, si * 2 + 2));
-        return st[si];
-    }
-
-    /* Function to construct segment tree from given array. This function 
-       allocates memory for segment tree and calls constructSTUtil() to 
-       fill the allocated memory */
-    void constructST(int arr[], int n) {
-        // Allocate memory for segment tree 
-
-        //Height of segment tree 
-        int x = (int) (Math.ceil(Math.log(n) / Math.log(2)));
-
-        //Maximum size of segment tree 
-        int max_size = 2 * (int) Math.pow(2, x) - 1;
-        st = new int[max_size]; // allocate memory 
-
-        // Fill the allocated memory st 
-        constructSTUtil(arr, 0, n - 1, 0);
-    }
-}
 //leetcode submit region end(Prohibit modification and deletion)
+// below solution is not efficient as it takes O(n^2) time to build the matrix,though the approach encountered is very beautiful; we could have used segment tree in it too for get min for a range
+//using segment tree complexity would be (log(n)*k)(FOR FINDING ANS)+ n*O(n)(FOR CREATION st)
+//WE CAN USE  THE APPROACH KIND OF SIMILAR WE USED IN MAX RECTANGLE IN BAR GRAPH
+//
+//
+//    int arr[]=new int[num.length()];
+//    char charArr[]= num.toCharArray();
+//          if(k==num.length())
+//                  return "0";
+//                  if(k==0)
+//                  return num;
+//                  for(int i=0; i < num.length();i++){
+//        arr[i]=charArr[i]-'0';
+//        }
+//
+//
+//        String result="";
+//        int start=0;
+//        int i=(k);
+//        while(arr.length-i>0){
+//
+//        //   System.out.println(i+" "+start+" "+" "+matrix[start][i]+" "+arr[matrix[start][i]]);
+//        result+=arr[minIndex(arr,start,i)];
+//        start=minIndex(arr,start,i)+1;
+//        i++;
+//        }
+//        return removeZero(result).length()==0?"0":removeZero(result);
+//
+//        }
+//public int minIndex(int[] arr,int left,int right){
+//        int min= left;
+//        for(int i=left;i<=right;i++){
+//        if (arr[i]<arr[min]){
+//        min=i;
+//        }
+//
+//        }
+//        return min;
+//        }
+
+//        }
+//
 	
